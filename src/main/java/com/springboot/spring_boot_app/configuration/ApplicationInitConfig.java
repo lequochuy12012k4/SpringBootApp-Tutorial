d315915b2e3810b284a1908a6a8af66e3684516f
Le Quoc Huy <lequochuy12012k4@gmail.com>
2026-07-08 22:03:11 +0700

    #20

--- src/main/java/com/springboot/spring_boot_app/configuration/ApplicationInitConfig.java
+++ src/main/java/com/springboot/spring_boot_app/configuration/ApplicationInitConfig.java
@@ -3,6 +3,7 @@ package com.springboot.spring_boot_app.configuration;
 import java.util.HashSet;
 
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,7 +26,9 @@ public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty(prefix = "spring",value = "datasource.driver-class-name",havingValue = "com.mysql.cj.jdbc.Driver")
     ApplicationRunner applicationRunner(UserRepository userRepositoty){
+        log.info("Init application");
         return args ->{
             if(userRepositoty.findByUsername("admin").isEmpty()){
                 var roles = new HashSet<String>();
---
+++ src/test/java/com/springboot/spring_boot_app/service/UserServiceTest.java
@@ -0,0 +1,92 @@
+package com.springboot.spring_boot_app.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.springboot.spring_boot_app.dto.request.UserCreationRequest;
+import com.springboot.spring_boot_app.entity.User;
+import com.springboot.spring_boot_app.exception.AppException;
+import com.springboot.spring_boot_app.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Slf4j
+@TestPropertySource("/test.properties")
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    private UserCreationRequest request;
+    private User userResponse;
+    private User user;
+
+    @BeforeEach
+    void initData(){
+        LocalDate dob = LocalDate.of(2004, 01, 12);
+        request = UserCreationRequest.builder()
+            .username("lequochuy")
+            .firstName("LeQuoc")
+            .lastName("Huy")
+            .password("123456789")
+            .doB(dob)
+            .build();
+            
+        userResponse = userResponse.builder()
+            .id("97f340ef")
+            .username("lequochuy")
+            .firstName("LeQuoc")
+            .lastName("Huy")
+            .doB(dob)
+            .build();
+        
+        user = User.builder()
+            .id("97f340ef")
+            .username("lequochuy")
+            .firstName("LeQuoc")
+            .lastName("Huy")
+            .doB(dob)
+            .build();
+    }
+
+    @Test
+    void createUser_validRequest_success(){
+        Mockito.when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.save(any())).thenReturn(user);
+
+        var response = userService.createUser(request);
+
+        Assertions.assertThat(response.getId()).isEqualTo("97f340ef");
+        Assertions.assertThat(response.getUsername()).isEqualTo("lequochuy");
+    }
+
+     @Test
+    void createUser_userExisted_fail(){
+        //GIVEN
+        Mockito.when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        //WHEN
+        var exception = assertThrows(AppException.class, ()->userService.createUser(request));
+        Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1002);
+        Assertions.assertThat(exception.getErrorCode().getMessage()).isEqualTo("User existed");
+    }
+}
